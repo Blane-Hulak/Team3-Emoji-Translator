f3336dfc14fbcab7ecdf0a37596018e91bc92fc2
samscha15 <147143114+samscha15@users.noreply.github.com>
2024-12-05 00:32:52 -0500

    Update Translate.java

--- Translate.java
+++ Translate.java
@@ -5,57 +5,59 @@
  * Description: This class wants to processe text, match substring with emoji table and replace them with emoji's.
  */
 
-//package emoji.translator;
+package emoji.translator;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Translate {
 
-    EmojiHandler emoji;
-    Inputhandler input;
+    private static InputHandler InputHandler = null;
+    EmojiManager emoji;
+    InputHandler input;
 
     // Default Constructor
     public Translate() {
-        this.emoji = new EmojiHandler();
+        this.emoji = new EmojiManager();
         this.input = new InputHandler();
 
     }
 
     // Constructor
-    public Translate(EmojiHandler emoji, InputHandler input) {
+    public Translate(EmojiManager emoji, InputHandler input, InputHandler InputHandler) {
         this.emoji = emoji;
-        this.inputHandler = inputHandler;
+        Translate.InputHandler = InputHandler;
     }
 
     // Getter for emojiHandler
-    public EmojiHandler getEmojiHandler() {
+    public EmojiManager getEmojiManager() {
         return emoji;
     }
 
     // Setter for emojiHandler
-    public void setEmojiHandler(EmojiHandler emoji) {
+    public void setEmojiHandler(EmojiManager emoji) {
         this.emoji = emoji;
     }
 
     // Getter for inputHandler
     public InputHandler getInputHandler() {
-        return inputHandler;
+        return InputHandler;
     }
 
     // Setter for inputHandler
     public void setInputHandler(InputHandler input) {
-        this.inputHandler = input;
+        Translate.InputHandler = input;
     }
 
     // Method to search for matching substring
-    public String compare(String text){
-        return text
+    public String compare(String text) {
+        return text;
     }
 
     // Method to perform translation
-    public String translated(String text) {
+    public String translated(String text, EmojiManager emoji) {
         // This method will use emojiHandler and inputHandler to translate text to emoji
         // language
-        return "The text" + text + "in emoji is:" + combo;
+        return "The text" + text + "in emoji is:" + emoji;
     }
 }
