f5b7639616d085ddf2eb4106944c33d9ce3db763
samscha15 <147143114+samscha15@users.noreply.github.com>
2024-12-05 00:25:46 -0500

    Update Translate.java

--- Translate.java
+++ Translate.java
@@ -3,47 +3,45 @@
  * Author: Sam Schaefer
  * Date: 12/04/2024
  * Description: This class wants to processe text, match substring with emoji table and replace them with emoji's.
-*/
+ */
 
-package emoji.translator;
+//package emoji.translator;
 
 import java.util.Scanner;
 
-
 public class Translate {
-    
+
     EmojiHandler emoji;
     Inputhandler input;
-    
 
-    // Default Constructor 
-    public Translate(){
-    this.emoji = new EmojiHandler();
-    this.input = new InputHandler();
-        
+    // Default Constructor
+    public Translate() {
+        this.emoji = new EmojiHandler();
+        this.input = new InputHandler();
+
     }
-    
+
     // Constructor
-    public Translate( EmojiHandler emoji, InputHandler input) {
+    public Translate(EmojiHandler emoji, InputHandler input) {
         this.emoji = emoji;
         this.inputHandler = inputHandler;
     }
-    
+
     // Getter for emojiHandler
     public EmojiHandler getEmojiHandler() {
         return emoji;
     }
-    
+
     // Setter for emojiHandler
     public void setEmojiHandler(EmojiHandler emoji) {
         this.emoji = emoji;
     }
-    
+
     // Getter for inputHandler
     public InputHandler getInputHandler() {
         return inputHandler;
     }
-    
+
     // Setter for inputHandler
     public void setInputHandler(InputHandler input) {
         this.inputHandler = input;
@@ -56,7 +54,8 @@ public class Translate {
 
     // Method to perform translation
     public String translated(String text) {
-        // This method will use emojiHandler and inputHandler to translate text to emoji language
+        // This method will use emojiHandler and inputHandler to translate text to emoji
+        // language
         return "The text" + text + "in emoji is:" + combo;
     }
 }
