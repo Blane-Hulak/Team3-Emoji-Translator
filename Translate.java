b3dd02c0e4cc6cfa8e17f05d18c191dad6452fff
samscha15 <147143114+samscha15@users.noreply.github.com>
2024-12-16 16:05:28 -0500

    Update Translate.java

--- Translate.java
+++ Translate.java
@@ -1,18 +1,12 @@
 /*
  * Project: Emoji Translator
  * Author: Sam Schaefer
- * Date: 12/04/2024
+ * Date: 12/16/2024
  * Description: This class wants to processe text, match substring with emoji table and replace them with emoji's.
  */
 
-package emoji.translator;
+public class Translate extends EmojiManager {
 
-import java.util.HashMap;
-import java.util.Scanner;
-
-public class Translate {
-
-    private static InputHandler InputHandler = null;
     EmojiManager emoji;
     InputHandler input;
 
@@ -24,40 +18,32 @@ public class Translate {
     }
 
     // Constructor
-    public Translate(EmojiManager emoji, InputHandler input, InputHandler InputHandler) {
+    public Translate(EmojiManager emoji, InputHandler input) {
         this.emoji = emoji;
-        Translate.InputHandler = InputHandler;
-    }
-
-    // Getter for emojiHandler
-    public EmojiManager getEmojiManager() {
-        return emoji;
-    }
-
-    // Setter for emojiHandler
-    public void setEmojiHandler(EmojiManager emoji) {
-        this.emoji = emoji;
-    }
-
-    // Getter for inputHandler
-    public InputHandler getInputHandler() {
-        return InputHandler;
-    }
-
-    // Setter for inputHandler
-    public void setInputHandler(InputHandler input) {
-        Translate.InputHandler = input;
-    }
-
-    // Method to search for matching substring
-    public String compare(String text) {
-        return text;
+        this.input = input;
     }
 
     // Method to perform translation
-    public String translated(String text, EmojiManager emoji) {
-        // This method will use emojiHandler and inputHandler to translate text to emoji
-        // language
-        return "The text" + text + "in emoji is:" + emoji;
+    public String translator(String text) {
+        String[] words = text.split(" ");
+        StringBuilder translatedText = new StringBuilder();
+        for (String word : words) {
+            boolean found = false;
+            String change = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+            if (emoji.containsKeyword(change)) {
+                translatedText.append(emoji.getEmoji(change)).append(word.replaceAll("[a-zA-Z0-9]", ""));
+
+                found = true;
+            }
+            if (!found) {
+
+                translatedText.append(word).append(" ");
+
+            }
+        }
+
+        return translatedText.toString().trim();
+
     }
 }
